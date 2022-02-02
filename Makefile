0c82d5d1892614cdc35a4be2f36debb26a4d31de
kizeevov <kizeev.ov@gmail.com>
2022-02-02 16:15:53 +0700

    Changed client to yew framework

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 .PHONY: client
 
+client-build:
+	cd client; yarn build
+
 client-browser:
 	cd client; yarn start
--- client/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-function Header() {
-    return (
-        <header/>
-    );
-}
-
-export default Header;
